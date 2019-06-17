a784f5acd8db9d3b77cab5c1a809cb85a3859290
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-17 21:04:16 +0530

    Create ki138.java

---
+++ ki138.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int pow=(int)(Math.log(n)/Math.log(2));  
+    if(Math.pow(2,pow)==n){
+      System.out.print("yes");
+    }
+    else{
+      System.out.print("no8");
+    }
+  }
+}
