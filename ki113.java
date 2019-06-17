7f80f4f8b5e4ddc870b4a7499decc9018958b6b6
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-17 06:49:23 +0530

    Create ki113.java

---
+++ ki113.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    try{
+    int n=in.nextInt();
+    }
+    catch(Exception e){
+      System.out.print("no");
+      System.exit(0);
+    }
+    System.out.print("yes");
+
+
+   
+   
+    
+      
+  }
+}
