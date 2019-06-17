fe780e2dfafc4c19264afaf9f02bf834a609bc0b
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-17 06:36:01 +0530

    Create ki110.java

---
+++ ki110.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=in.nextInt();
+    }
+    int s=0;
+    for(int i=n-1;i>=0;i--){
+      s+=arr[i];
+    }
+    
+        for(int i=0;i<n;i++){
+        System.out.print((arr[i]+s)+" ");
+        }
+
+
+   
+   
+    
+      
+  }
+}
