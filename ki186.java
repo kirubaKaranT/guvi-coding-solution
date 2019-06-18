3dab494a6c2fe6924d8493471b0d08529afdea5e
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-18 10:41:19 +0530

    Create ki186.java

---
+++ ki186.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+  static boolean[] arr;
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+   String s=in.nextLine();
+   String a="Vishal";
+    String b="Sundar";
+    if(s.contains(a)&&s.contains(b))
+    System.out.print("yes");
+    else
+    System.out.print("no");
+    
+  }
+
+}
