58c327c9b9c152c4b92ebdb44bc883c6e7eb336d
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-18 08:50:10 +0530

    Update ki181.java

--- ki181.java
+++ ki181.java
@@ -1,14 +1,28 @@
 import java.util.*;
 
 public class Main {
-  
+  static boolean[] arr;
   public static void main(String[] args) {
     Scanner in=new Scanner(System.in);
     int n=in.nextInt();
-   if(n%3==0||n%7==0||(n%7)%3==0)
+   
+    
+    
+   if(find( n))
     System.out.print("yes");
     else
     System.out.print("no");
     
+  }
+  public static boolean find(int n){
+    if(n==0){
+      return true;
+    }
+    else if(n<0){
+      return false;
+    }
+    else{
+      return find(n-3)||find(n-7);
+    }
   }
 }
