e51f128803d78ddc6932be3d0e207b479dad7db8
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-18 18:46:38 +0530

    Create ki153.java

---
+++ ki153.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    String s=in.next();
+    int k=in.nextInt();
+    
+    for(int i=0;i<s.length();i++){
+    if((i+1)%k==0){
+    System.out.print(s.charAt(i)+" ");
+    }
+    }
+  
+  }
+}
