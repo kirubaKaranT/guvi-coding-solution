8ba7e2510d972405d04c5fb8ba12b9c265edb192
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-18 19:10:32 +0530

    Create ki157.java

---
+++ ki157.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int  n=in.nextInt();
+    int k=in.nextInt();
+    n*=k;
+    int c=1;
+    while(n>0){
+      
+      if((n&1)==1){
+        System.out.print(c);
+        System.exit(0);
+             
+      }
+      n/=2;
+     c++;
+    }
+  
+  }
+}
