3ff6a172b378140cb4a0c03c6dd3ec929a40215b
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-19 12:12:21 +0530

    Create ki128.java

---
+++ ki128.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    String s=in.nextLine();
+    for(int i=0;i<s.length();i++){
+      if(s.charAt(i)!=' '){
+        System.out.print(s.charAt(i));
+      }
+      
+    }
+  }
+  
+}
