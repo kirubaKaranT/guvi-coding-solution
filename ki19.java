adbefd17b9b4e031224f59e74a41b18375eeb688
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-19 11:45:09 +0530

    Create ki19.java

---
+++ ki19.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+   int n=in.nextInt();
+   for(int i=2;i<n;i++){
+     if(isprime(i)&&n%i==0)
+     System.out.print(i+" ");
+   }
+    
+  
+  }
+  static boolean isprime(int n){
+	   boolean prime=true;
+    for(int i=2;i<=Math.sqrt(n);i++){
+        if(n%i==0){
+            prime=false;
+        }
+    }
+  
+    return prime;
+	}
+  
+}
