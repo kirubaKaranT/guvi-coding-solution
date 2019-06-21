8bab538efa29a616c7c533f360db2d45113137e2
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-06-21 11:16:49 +0530

    Rename ki128.java to k128.java

---
+++ k128.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+   
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=in.nextInt();
+    }
+ boolean odd=true;
+   for(int i=0;i<n;i++){
+    if(arr[i]%2==0){
+      odd=false;
+    }
+   }
+
+  if(!odd){
+    System.out.print("yes");
+  }
+  else{
+    System.out.print("no");
+  }
+  }
+}
