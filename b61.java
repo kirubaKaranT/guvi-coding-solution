5117fcadfac5e584dece442a0eef660a0f0d6855
kirubaKaranT <36567808+kirubaKaranT@users.noreply.github.com>
2019-07-11 08:36:22 +0530

    Create b61.java

---
+++ b61.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+    String s=in.next();
+    int n=in.nextInt();
+    System.out.println(s.substring(0,n));
+	}
+}
